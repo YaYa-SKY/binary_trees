8cd36c36b822d0c520f72613330db4bdd644ecb5
YaYa-SKY <yahia.seqqai@gmail.com>
2023-10-31 04:20:15 -0700

    updated

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,50 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - Counts nodes with at least one child in a binary tree
+ * @tree: Pointer to the binary tree to count nodes in
+ *
+ * Return: Number of nodes with at least one child
+ *         0 if the tree is NULL
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+    if (!tree || (!tree->left && !tree->right))
+        return (0);
+
+    return (binary_tree_size(tree) - binary_tree_leaves(tree));
+}
+
+/**
+ * binary_tree_size - Measures the size of a binary tree
+ * @tree: Pointer to the binary tree to measure
+ *
+ * Return: Size of the tree
+ *         0 if the tree is NULL
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+    if (!tree)
+        return (0);
+
+    return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+}
+
+/**
+ * binary_tree_leaves - Counts the leaves in a binary tree
+ * @tree: Pointer to the binary tree to count leaves in
+ *
+ * Return: Number of leaves
+ *         0 if the tree is NULL
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+    if (!tree)
+        return (0);
+
+    if (!tree->left && !tree->right)
+        return (1);
+
+    return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
+}
+
