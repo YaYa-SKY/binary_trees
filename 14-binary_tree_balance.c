29a42e6b952a315c99bba9d99374dcaf1da4f1d1
YaYa-SKY <yahia.seqqai@gmail.com>
2023-10-31 05:01:07 -0700

    betty style error fixed

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,15 +9,15 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    int height_l, height_r;
+	int height_l, height_r;
 
-    if (!tree)
-        return (0);
+	if (!tree)
+		return (0);
 
-    height_l = tree->left ? (int)binary_tree_height(tree->left) : -1;
-    height_r = tree->right ? (int)binary_tree_height(tree->right) : -1;
+	height_l = tree->left ? (int)binary_tree_height(tree->left) : -1;
+	height_r = tree->right ? (int)binary_tree_height(tree->right) : -1;
 
-    return (height_l - height_r);
+	return (height_l - height_r);
 }
 
 /**
@@ -29,14 +29,14 @@ int binary_tree_balance(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t height_l = 0;
-    size_t height_r = 0;
+	size_t height_l = 0;
+	size_t height_r = 0;
 
-    if (!tree)
-        return (0);
+	if (!tree)
+		return (0);
 
-    height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-    height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
-    return (height_l > height_r ? height_l : height_r);
+	return (height_l > height_r ? height_l : height_r);
 }
