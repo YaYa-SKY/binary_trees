053a5e051f20498a9168c0d44b888386ea75667a
YaYa-SKY <yahia.seqqai@gmail.com>
2023-10-31 04:23:12 -0700

    updated

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -2,10 +2,10 @@
 
 /**
  * binary_tree_nodes - Counts nodes with at least one child in a binary tree
- * @tree: Pointer to the binary tree to count nodes in
+ * @tree: Pointer to the binary tree to count nodes from
  *
- * Return: Number of nodes with at least one child
- *         0 if the tree is NULL
+ * Return: Number of nodes counted
+ *         0 if tree is NULL
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
@@ -17,10 +17,10 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 
 /**
  * binary_tree_size - Measures the size of a binary tree
- * @tree: Pointer to the binary tree to measure
+ * @tree: Pointer to the binary tree to measure the size of
  *
  * Return: Size of the tree
- *         0 if the tree is NULL
+ *         0 if tree is NULL
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
@@ -32,10 +32,10 @@ size_t binary_tree_size(const binary_tree_t *tree)
 
 /**
  * binary_tree_leaves - Counts the leaves in a binary tree
- * @tree: Pointer to the binary tree to count leaves in
+ * @tree: Pointer to the binary tree to count leaves from
  *
  * Return: Number of leaves
- *         0 if the tree is NULL
+ *         0 if tree is NULL
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
@@ -45,6 +45,6 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
     if (!tree->left && !tree->right)
         return (1);
 
-    return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
+    return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
 
